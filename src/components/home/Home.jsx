20e5fb0f9e129789c019c20fa01696ce196730e0
siam666-shell <abdullahsiam009@gmail.com>
2022-10-03 23:24:37 +0600

    search section added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,6 +5,32 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="main-content">
+        <div className="container">
+          <div className="row">
+            <div className="col-8">
+              <h3>Article</h3>
+            </div>
+            <div className="col-4">
+              <div className="search-category-tag">
+                <div className="search">
+                  <h2>Search</h2>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Search"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <button className="btn btn-block">Search</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
